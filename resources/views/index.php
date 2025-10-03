58112c3151f0f827c54598c9f4252bc873f26f4e
ocidaja <rosidandrian1234@gmail.com>
2025-10-03 10:05:39 +0800

    tambah style

--- resources/views/index.php
+++ resources/views/index.php
@@ -5,6 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+    <style>
+        h1 {
+            color:red;
+        }
+    </style>
 <body>
     <h1>Welcome to the Index Page</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, ex.</p>
